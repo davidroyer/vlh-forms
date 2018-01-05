d77a2c98ab367c1bf49ca969fe430782ff2a05a5
David Royer <droyer01@gmail.com>
2018-01-05 00:33:55 -0500

    update

--- src/components/FormStep.vue
+++ src/components/FormStep.vue
@@ -46,73 +46,73 @@ export default {
 </script>
 
 <style lang="scss">
-.step {
-  position: relative;
-  min-height: 100px;
-}
-.stepFormProgress {
-  display: flex;
-  justify-content: space-around;
-  justify-content: space-between;
-  flex-flow: row wrap;
-  margin: 1em 0;
-
-  .stepProgress {
-    padding: .5em;
-    transition: .25s ease;
+  .step {
     position: relative;
-    border-bottom: 3px solid transparent;
-
-    &.currentStep {
-      border-bottom: 2px solid #444;
-    }
-
-    &.completedStep {
-
-      &:after {
-        content: "\2713";
-        font-size: 1.5em;
-        font-weight: 500;
-        position: absolute;
-        left: 100%;
-        bottom: 2px;
-        transition: .4s ease;
-        color: #444;
-      }
-    }
+    min-height: 100px;
   }
-}
-
-.light {
   .stepFormProgress {
-    color: #f9f9f9 !important;
+    display: flex;
+    justify-content: space-around;
+    justify-content: space-between;
+    flex-flow: row wrap;
+    margin: 1em 0;
 
     .stepProgress {
+      padding: .5em;
+      transition: .25s ease;
+      position: relative;
+      border-bottom: 3px solid transparent;
 
       &.currentStep {
-        border-bottom: 2px solid #f9f9f9;
+        border-bottom: 2px solid #444;
       }
-      &.completedStep:after {
-        color: #f9f9f9;
+
+      &.completedStep {
+
+        &:after {
+          content: "\2713";
+          font-size: 1.5em;
+          font-weight: 500;
+          position: absolute;
+          left: 100%;
+          bottom: 2px;
+          transition: .4s ease;
+          color: #444;
+        }
       }
     }
-
   }
-}
 
-.dark {
-  color: #222 !important;
+  .light {
+    .stepFormProgress {
+      color: #f9f9f9 !important;
 
-  .stepFormProgress {
+      .stepProgress {
+
+        &.currentStep {
+          border-bottom: 2px solid #f9f9f9;
+        }
+        &.completedStep:after {
+          color: #f9f9f9;
+        }
+      }
+
+    }
+  }
+
+  .dark {
     color: #222 !important;
 
-    .stepProgress {
+    .stepFormProgress {
+      color: #222 !important;
 
-      &.currentStep {
-        border-bottom: 2px solid #222;
+      .stepProgress {
+
+        &.currentStep {
+          border-bottom: 2px solid #222;
+        }
       }
-    }
 
+    }
   }
-}
 </style>
